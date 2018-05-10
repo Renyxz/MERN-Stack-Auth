c1a77d3df223a6a795d1a4af237e6e131855b440
Ren <ren.yxz.dev@gmail.com>
2018-05-09 20:01:38 -0400

    Added profile dropdown button and hide/show logic

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,6 +4,10 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
 
+    const isAuthenticated = window.sessionStorage.token;
+    const authVisibility = isAuthenticated ? 'auth-hide' : 'auth-show';
+    const profileVisibility = isAuthenticated ? 'profile-hide' : 'profile-show';
+
     return(
 
         <div>
@@ -14,13 +18,14 @@ const Navbar = () => {
                     <i className="fab fa-react mr-2"></i>
                     MERN Auth
                 </Link>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
+
+                <button className={ `navbar-toggler ${ authVisibility }` } type="button" data-toggle="collapse" data-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
 
                 <div className="collapse navbar-collapse" id="navbarColor03">
 
-                    <ul className="navbar-nav ml-auto">
+                    <ul className={ `navbar-nav ml-auto ${ authVisibility }` }>
                         
                         <li className="nav-item">
                             <Link className="nav-link" to="/signin">Sign in</Link>
@@ -32,6 +37,21 @@ const Navbar = () => {
 
                     </ul>
                     
+                </div>
+
+                <div className={ `profile btn-group ${ profileVisibility }` }>
+                    <button type="button" className="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <img className="img-fluid rounded-circle" width="35" height="35" src="http://via.placeholder.com/150x150" alt="profile"/>
+                    </button>
+                    <div className="dropdown-menu dropdown-menu-right">
+                        <Link className="dropdown-item" to="/dashboard" >
+                            Dashboard
+                        </Link>
+                        
+                        <button className="dropdown-item" type="button">
+                            Sign out
+                        </button>
+                    </div>
                 </div>
 
             </nav>
